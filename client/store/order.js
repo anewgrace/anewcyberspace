e0dfc9018579eaf7c1785171290e7d2767ed0851
kmshoell <34169711+kmshoell@users.noreply.github.com>
2020-11-16 21:33:07 -0500

    Added redux store logic for update/remove orderItem

--- client/store/order.js
+++ client/store/order.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 //-------------ACTION TYPES----------------
 const ADD_ORDER_ITEM = 'ADD_ORDER_ITEM'
 const SET_CART = 'SET_CART'
+const UPDATE_QUANTITY = 'UPDATE_QUANTITY'
+const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM'
 //-------------ACTION CREATORS----------------
 const addOrderItem = orderItem => ({
   type: ADD_ORDER_ITEM,
@@ -10,6 +12,16 @@ const addOrderItem = orderItem => ({
 const setCart = cart => ({
   type: SET_CART,
   cart
+})
+
+const updateCartItem = cartItem => ({
+  type: UPDATE_QUANTITY,
+  cartItem
+})
+
+const removeCartItem = cartItem => ({
+  type: REMOVE_CART_ITEM,
+  cartItem
 })
 //-------------THUNKS----------------
 export function addOrderItemToCart(orderItem) {
@@ -35,13 +47,55 @@ export function retrieveCart() {
   }
 }
 
+//@param cartItem is an object containing cartItem ID and Quantity
+export function putCartItem(cartItem) {
+  return async dispatch => {
+    try {
+      const {data} = await axios.put(`/api/cart/${cartItem.id}`, {
+        quantity: cartItem.quantity
+      })
+      console.log('data (returns 1 if updated successfully)', data[0])
+      data[0] && dispatch(updateCartItem(cartItem))
+    } catch (error) {
+      console.log('ERROR IN UPDATE CART THUNK:', error)
+    }
+  }
+}
+//@param cartItem is an object containing cartItem ID and Quantity
+export function deleteCartItem(cartItem) {
+  return async dispatch => {
+    try {
+      const {data} = await axios.delete(`/api/cart/${cartItem.id}`)
+      console.log('data (returns 1 if deleted successfully)', data[0])
+      data[0] === 1 && dispatch(removeCartItem(cartItem))
+    } catch (error) {
+      console.log('ERROR IN DELETE CART THUNK:', error)
+    }
+  }
+}
+
 //-------------REDUCER----------------
 export default function orderReducer(state = {}, action) {
   switch (action.type) {
     case ADD_ORDER_ITEM:
-      return {...state, orderItem: [...state.orderItem, action.orderItem]}
+      return {...state, orderItems: [...state.orderItems, action.orderItem]}
     case SET_CART:
       return action.cart
+    case UPDATE_QUANTITY:
+      return {
+        ...state,
+        orderItems: state.orderItems.map(item => {
+          if (action.orderItem.id === item.id) return action.orderItem
+          else return item
+        })
+      }
+    case REMOVE_CART_ITEM:
+      return {
+        ...state,
+        orderItems: state.orderItems.filter(
+          item => action.orderItem.id !== item.id
+        )
+      }
     default:
       return state
   }
