917a2dc4ca654dea81c1e0ebd6f38551ac2f21c2
Jess <jessicaspiezio07@gmail.com>
2020-11-17 11:05:03 -0500

    Fixed images for heroku

--- client/components/Products/ProductTile.js
+++ client/components/Products/ProductTile.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProductTile({imageUrl, name, price, description}) {
   return (
     <div className="product-tile" id="productTile">
-      <img src={'../' + imageUrl} />
+      <img src={imageUrl} />
       <h3 className="product-name">{name}</h3>
       <p className="product-description">{description}</p>
       <h3 className="product-price">${(price / 100).toLocaleString()}</h3>
